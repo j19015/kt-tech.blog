1d52644cc3d5b99bff6cb99d86de671db1c21940
j19015 <j19015@sangi.jp>
2023-09-03 19:32:56 +0900

    src/app/blog/page.tsxの処理を変更

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,33 @@
+// pages/index.js
+import Link from "next/link";
+import { client } from "@/../libs/client"
+
+// 型定義
+interface HomeProps {
+  blog: { id: string; title: string; body: string; }[];
+}
+
+
+export default function Home({ blog }: HomeProps) {
+  return (
+    <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
