3daa55de0b3e516bf2c6f40578f21c657420383c
j19015 <j19015@sangi.jp>
2023-09-04 19:47:57 +0900

    src/app/blogs/[id]/page.tsx エラー解決

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -5,34 +5,32 @@ import { Blog} from "@/../interface/blog"
 import { useParams} from 'next/navigation';
 import { useEffect,useState } from "react";
 
+export default () => {
 
+  //URLパラメータ取得用のparamsを定義
+  const params = useParams()
 
-export default async () => {
+  //id抽出
+  const {id} = params
 
   //状態管理変数
   const [blog,setBlog] = useState<Blog | null>(null);
-  const [id,setId] = useState<Number | null>(null);
 
   useEffect(()=>{
-    //fetchBlog();
+    fetchBlog();
   },[])
 
   const fetchBlog = async() => {
-    //URLパラメータ取得用のparamsを定義
-    const params = useParams()
-    //paramsからidを抜き出し
-    setId(Number(params.id))
     try{
-      console.log(process.env.API_KEY)
       //APIリクエスト
-      const data = await client.get({ endpoint: "blog",contentId: id?.toString() });
+      const data :Blog = await client.get({ endpoint: 'blog',contentId: id?.toString() });
+
       //コンテンツ抽出
-      setBlog(data.contents);
-      console.log(data)
+      setBlog(data);
+
     }catch(e){
       console.log(e);
     }
-    console.log(process.env.API_KEY);
   }
 
   //表示内容
