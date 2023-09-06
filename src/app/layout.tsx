66a1a19d287fe0aea6b14c6dd71ba4065a432a23
j19015 <j19015@sangi.jp>
2023-09-06 12:25:57 +0900

    Header.tsx,Footer.tsxに最低限のレイアウトを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Header } from '../../src/components/Header/Header'
+import { Footer } from '../../src/components/Footer/Footer'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   )
 }
