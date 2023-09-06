e7b600356052cedd4f69c96d9e06a0755b1c9b6a
j19015 <j19015@sangi.jp>
2023-09-06 20:42:17 +0900

    src/app/page.tsxのレイアウトの領域を設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <>
       <div className='h-screen'>
-
+        <div className="h-full grid grid-cols-12 gap-2 grid-flow-row">
+          <div className='col-span-12 md:col-span-8 m-4 rounded-lg bg-gray-300'>
+            main
+          </div>
+          <div className='col-span-12 md:col-span-4 m-4 rounded-lg bg-gray-300'>
+            Sidebar
+            <div className='bg-gray-500 h-1/4 rounded-lg m-4'>
+              注目記事
+            </div>
+            <div className='bg-gray-500 h-1/4 rounded-lg m-4'>
+              最新記事
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
