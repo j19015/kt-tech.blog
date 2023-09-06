fc0f628932f4b0c25ca94c1382cc882cc7d7e48f
j19015 <j19015@sangi.jp>
2023-09-06 19:53:20 +0900

    Footer.tsxのレイアウトを変更

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,30 @@
+import Link from 'next/link';
 import React from 'react';
 
-export const Footer: React.FC = () => {
+export const Footer = () => {
   return (
-    <footer className="bg-blue-500 p-4 mt-8">
-      <div className="container mx-auto">
-        {/* フッターのコンテンツをここに追加 */}
-        <p className="text-white">© 2023 技術ブログ</p>
+      
+  <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">kt-tech.blog</Link>. All Rights Reserved.
+      </span>
+      <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+              <Link href="#" className="mr-4 hover:underline md:mr-6 ">About</Link>
+          </li>
+          <li>
+              <Link href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
+          </li>
+          <li>
+              <Link href="#" className="mr-4 hover:underline md:mr-6">Licensing</Link>
+          </li>
+          <li>
+              <Link href="#" className="hover:underline">Contact</Link>
+          </li>
+      </ul>
       </div>
-    </footer>
+  </footer>
+
   );
 };
 
