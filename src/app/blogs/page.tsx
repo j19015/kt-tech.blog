40190eaf0b968b8f8c78e0a1dfa788fb23e44d42
j19015 <j19015@sangi.jp>
2023-09-06 21:05:26 +0900

    src/app/blogs/page.tsxに最低限のレイアウトの枠を設定

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -13,17 +13,41 @@ export default async function StaticPage(){
 
   return (
     <>
-      <div>
-        <h1>{time}</h1>
-        <ul>
-          {contents.map((blog)=>{
-            return (
-              <li key={blog.id}>
-                <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
-              </li>
-            )
-          })}
-        </ul>
+      <h1>{time}</h1>
+      <div className='h-screen'>
+        <div className="h-full grid grid-cols-12 gap-2 grid-flow-row">
+          <div className='col-span-12 md:col-span-8 m-4 rounded-lg bg-gray-300'>
+            main
+            {contents.map((blog)=>{
+              return (
+                <>
+                  <div className='bg-gray-500 h-1/6 rounded-lg m-4'>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.body}</p>
+                  </div>
+                </>
+              )
+            })}
+          </div>
+          <div className='col-span-12 md:col-span-4 m-4 rounded-lg bg-gray-300'>
+            Sidebar
+            <div className='bg-gray-500 h-1/6 rounded-lg m-4'>
+              検索Form
+            </div>
+            <div className='bg-gray-500 h-1/6 rounded-lg m-4'>
+              注目記事
+            </div>
+            <div className='bg-gray-500 h-1/6 rounded-lg m-4'>
+              最新記事
+            </div>
+            <div className='bg-gray-500 h-1/6 rounded-lg m-4'>
+              カテゴリー
+            </div>
+            <div className='bg-gray-500 h-1/6 rounded-lg m-4'>
+              アーカイブ
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
