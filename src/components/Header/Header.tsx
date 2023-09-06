50749f14d0e30874b85457f96bf22c2a72eb0674
j19015 <j19015@sangi.jp>
2023-09-06 19:35:44 +0900

    Header.tsxのレイアウト変更

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,56 +1,92 @@
-import Link from 'next/link';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isOpen, setOpen] = useState<boolean>(false);
 
-//   const toggleMenu = () => {
-//     const menu = document.getElementById('menu');
-//     if (menu) {
-//         menu.classList.toggle('hidden');
-//     }
-//   };
+  const handleMenuOpen = () => {
+    setOpen(!isOpen);
+  };
+
+  const handleMenuClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <header className="bg-blue-500 p-4">
-      <nav className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-2xl font-semibold">技術ブログ</h1>
-        <div className="lg:hidden">
-          <button
-            className="text-white hover:text-gray-300 focus:outline-none"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="lg:flex lg:items-center" id="menu">
-          <Link href="/" className="text-white hover:text-gray-300 px-4 py-2 block lg:inline">
-              Home
-          </Link>
-          <Link href="/blogs" className="text-white hover:text-gray-300 px-4 py-2 block lg:inline">
-              Blog
-          </Link>
-          <Link href="/dev" className="text-white hover:text-gray-300 px-4 py-2 block lg:inline">
-              PF
-          </Link>
-          <Link href="/introduction" className="text-white hover:text-gray-300 px-4 py-2 block lg:inline">
-              Introduction
-          </Link>
-          {/* 他のリンクを追加 */}
-        </div>
+    <header className="py-6 px-4 flex justify-between items-center bg-blue-100">
+      <Link className="z-50" href="/" onClick={handleMenuClose}>
+        <h1 className="text-2xl font-serif">kt-tech.blog</h1>
+      </Link>
+
+      <nav
+        className={
+          isOpen
+            ? "z-40 bg-blue-100 fixed top-0 right-0 bottom-0 left-0 h-screen flex flex-col"
+            : "fixed right-[-100%] md:right-4"
+        }
+      >
+        <ul
+          className={
+            isOpen
+              ? "flex h-screen justify-center items-center flex-col gap-6 text-xl"
+              : "block md:flex md:gap-8"
+          }
+        >
+          <li>
+            <Link onClick={handleMenuClose} href="/">
+              <span className="font-serif">
+                Home
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleMenuClose} href="/blogs">
+              <span className="font-serif">
+                Blog
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleMenuClose} href="/dev">
+              <span className="font-serif">
+                Dev
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleMenuClose} href="/introduction">
+              <span className="font-serif">
+                Introduction
+              </span>
+            </Link>
+          </li>
+        </ul>
       </nav>
+      <button className="z-50 space-y-2 md:hidden" onClick={handleMenuOpen}>
+        <span
+          className={
+            isOpen
+              ? "block w-8 h-0.5 bg-gray-600 translate-y-2.5 rotate-45 duration-300"
+              : "block w-8 h-0.5 bg-gray-600 duration-300"
+          }
+        />
+        <span
+          className={
+            isOpen ? "block opacity-0 duration-300" : "block w-8 h-0.5 bg-gray-600 duration-300"
+          }
+        />
+        <span
+          className={
+            isOpen
+              ? "block w-8 h-0.5 bg-gray-600 -rotate-45 duration-300"
+              : "block w-8 h-0.5 bg-gray-600 duration-300"
+          }
+        />
+      </button>
     </header>
   );
-};
+}
 
 export default Header;
