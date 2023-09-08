c3a2f4eab180bb9c218359deb2a3fa81a26df729
j19015 <j19015@sangi.jp>
2023-09-08 17:49:27 +0900

    src/app/dev/page.tsx Sidebarを表示する記述を追加

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import Sidebar from '@/components/SIdebar/Sidebar';
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
           <div className='col-span-12 md:col-span-8 m-4 rounded-lg bg-gray-300'>
             <h1>開発中のものやPFのページになる予定</h1>
           </div>
+          <Sidebar/>
         </div>
       </div>
     </>
