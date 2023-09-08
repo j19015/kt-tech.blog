55efd6c85eff396f9306df21b393a6113aaefa2f
j19015 <j19015@sangi.jp>
2023-09-08 15:23:50 +0900

    markdown風のレイアウトを追加

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -3,6 +3,9 @@ import Link from "next/link"
 import Image from "next/image"
 import parse from "html-react-parser"
 import { getDetail,getList } from "../../../../libs/microcms"
+import cheerio from "cheerio";
+import hljs from "highlight.js";
+import "highlight.js/styles/hybrid.css";
 
 export async function generateStaticParams(){
   const { contents } = await getList();
@@ -22,6 +25,15 @@ export default async function StaticDetailPage({
 }) {
   const blog = await getDetail(blogId);
 
+  const body = cheerio.load(blog.body);
+  body("pre code").each((_, elm) => {
+    const result = hljs.highlightAuto(body(elm).text());
+    body(elm).html(result.value);
+    body(elm).addClass("hljs");
+  });
+  console.log(blog.body)
+  console.log(body.html())
+
   //ページの生成された時間を取得
   const time = new Date().toLocaleString();
 
@@ -36,22 +48,22 @@ export default async function StaticDetailPage({
     <div className='h-screen'>
       <div className="h-full grid grid-cols-12 gap-2 grid-flow-row">
         <div className='col-span-12 md:col-span-8 m-4 rounded-lg bg-gray-300'>
+          
           main
           <div className="p-4">
             <Link href={`/blogs/${blog.id}`}>
               <Image 
                 src={ blog.eyecatch?.url ? blog.eyecatch?.url : `../../../public/images/no_image`}
                 alt="画像"
-                width={1000}
-                height={1000}
+                width={10000}
+                height={10000}
                 className="rounded-lg"
               />
             </Link>
           </div>
-          <div className="p-4">
+          <div className="p-4 markdown">
             <h1>{blog.title}</h1>
-            <div>
-            {parse(blog.body)}
+            <div dangerouslySetInnerHTML={{ __html: body.html() }}>
             </div>
           </div>
         </div>
