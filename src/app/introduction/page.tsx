4e8eadff362f733e5c315f34a1eae65b42309821
j19015 <j19015@sangi.jp>
2023-09-08 17:54:14 +0900

    src/app/introduction/page.tsx レイアウト微修正

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div>
         <div className="grid grid-cols-12 gap-2 grid-flow-row">
           <div className='col-span-12 md:col-span-8 m-4 rounded-lg bg-gray-300'>
-            <h1>開発中のものやPFのページになる予定</h1>
+            <h1>自己紹介ページになる予定</h1>
           </div>
           <Sidebar/>
         </div>
