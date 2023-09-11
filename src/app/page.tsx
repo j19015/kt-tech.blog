fea66d81353228dad0a74743e25d33e817b3bf32
j19015 <j19015@sangi.jp>
2023-09-11 12:57:14 +0900

    src/app/page.tsx レイアウトのレスポンシブの部分をブログ一覧と統一

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,28 +16,34 @@ export default async function StaticPage() {
 
   return (
     <>
-      <h1>{time}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"> {/* グリッドを設定 */}
-        {contents.map((blog) => {
-          return (
-            <div key={blog.id}>
-              <div className="bg-white border border-gray-200 rounded-lg shadow-md">
-                <a href={`/blogs/${blog.id}`}>
-                  <img className="rounded-t-lg w-full" src={blog.eyecatch?.url} alt="" />
-                </a>
-                <div className="p-5">
-                  <a href={`/blogs/${blog.id}`}>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 truncate">
-                      {blog.title}
-                    </h5>
-                  </a>
+    <h1>{time}</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4"> {/* グリッドを設定 */}
+        <div className="lg:col-span-2"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {contents.map((blog) => {
+              return (
+                <div key={blog.id} className="w-full p-2 md:w-full lg:w-full">
+                  <div className="bg-white border border-gray-200 rounded-lg shadow-md">
+                    <a href={`/blogs/${blog.id}`}>
+                      <img className="rounded-t-lg w-full  max-w-full" src={blog.eyecatch?.url} alt="" /> {/* 画像の高さと幅を指定 */}
+                    </a>
+                    <div className="p-5">
+                      <a href={`/blogs/${blog.id}`}>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 truncate">
+                          {blog.title}
+                        </h5>
+                      </a>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })}
+          </div>
+        </div>
+        <div className="lg:col-span-1"> {/* 通常の画面サイズでは1列分のスペースを占有 */}
+          <Sidebar />
+        </div>
       </div>
-      <Sidebar />
     </>
   );
 }
