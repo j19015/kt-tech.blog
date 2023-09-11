facd898eb9d5ce61e95cb58016201c7ed3d64fed
j19015 <j19015@sangi.jp>
2023-09-11 19:15:12 +0900

    pagination 全画面時にレイアウト崩れを修正

--- src/app/blogs/page/[pageId]/page.tsx
+++ src/app/blogs/page/[pageId]/page.tsx
@@ -59,7 +59,7 @@ export default async function StaticPaginationPage({
     return (
       <>
         <h1>{time}</h1>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4"> {/* グリッドを設定 */}
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 pb-8"> {/* グリッドを設定 */}
           <div className="lg:col-span-2"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {contentSlice.map((blog) => {
