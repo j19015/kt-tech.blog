f13f2d9a2104dc3de0e5f5d84e7acfbf75ed980a
j19015 <j19015@sangi.jp>
2023-09-13 15:46:46 +0900

    ブログの一覧ページにtitleを追加

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -7,19 +7,18 @@ export default async function StaticPage() {
   const { contents } = await getList();
   console.log(contents);
 
-  // ページの生成された時間を取得
-  const time = new Date().toString();
-
   if (!contents || contents.length === 0) {
     return <h1>No Contents</h1>;
   }
 
   return (
     <>
-      <h1>{time}</h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4"> {/* グリッドを設定 */}
         <div className="lg:col-span-2"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="text-center mt-1 w-full col-span-2">
+              <h2 className="lg:text-5xl md:text-4xl text-3xl font-extrabold text-indigo-900 mb-6 underline">Blog</h2>
+            </div>
             {contents.map((blog) => {
               return (
                 <div key={blog.id} className="w-full p-2 md:w-full lg:w-full">
