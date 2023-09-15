272442a905f8f1df253bd47fd7f84ea015692f1c
j19015 <j19015@sangi.jp>
2023-09-15 18:53:59 +0900

    前のコミットの微修正 && sidebarのformを置き換え

--- src/components/SIdebar/Sidebar.tsx
+++ src/components/SIdebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import Form from '../Form/Form';
 import { getList, getTagList, getCategoryList } from '../../../libs/microcms';
 
 export const Sidebar = async () => {
@@ -15,25 +16,7 @@ export const Sidebar = async () => {
 
     return (
         <div className="col-span-12 md:col-span-4 m-4 rounded-lg bg-gray-300 p-6">
-            <div className="mb-6">
-                <h3 className="text-xl font-bold mb-4">検索フォーム</h3>
-                <form className="bg-white rounded-lg shadow-md p-4">
-                    <div className="flex items-center border-b border-b-2 border-gray-300 py-2">
-                        <input
-                            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                            type="text"
-                            placeholder="キーワードを入力"
-                            aria-label="検索フォーム"
-                        />
-                        <button
-                            className="flex-shrink-0 bg-indigo-500 hover:bg-indigo-700 border-indigo-500 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded"
-                            type="button"
-                        >
-                            検索
-                        </button>
-                    </div>
-                </form>
-            </div>
+            <Form/>
 
             <div className="mb-6">
                 <h3 className="text-xl font-bold mb-4">最新記事</h3>
