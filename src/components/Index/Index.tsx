d4df0d7ec0c7dc476f0e5af78c67ac7e1504d94e
j19015 <j19015@sangi.jp>
2023-09-18 04:00:40 +0900

    template化する部分を変更

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -4,61 +4,63 @@ import Link from "next/link";
 export const Index = ({ contents }: BlogProps) => {
   return (
     <>
-      {contents.map((blog) => {
-        return (
-          <div key={blog.id} className="w-full p-2 md:w-full lg:w-full">
-            <div className="bg-white border border-gray-200 rounded-lg shadow-md">
-              <Link href={`/blogs/${blog.id}`}>
-                <img
-                  className="rounded-t-lg w-full max-w-full"
-                  src={blog.eyecatch?.url}
-                  alt=""
-                  style={{ boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)" }}
-                />
-              </Link>
-              <div className="p-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {contents.map((blog) => {
+            return (
+            <div key={blog.id} className="w-full p-2 md:w-full lg:w-full">
+                <div className="bg-white border border-gray-200 rounded-lg shadow-md">
                 <Link href={`/blogs/${blog.id}`}>
-                  <h5 className="mb-2 lg:text-lg font-bold tracking-tight text-gray-900 truncate md:text-base sm:text-base">
-                    {blog.title}
-                  </h5>
+                    <img
+                    className="rounded-t-lg w-full max-w-full"
+                    src={blog.eyecatch?.url}
+                    alt=""
+                    style={{ boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)" }}
+                    />
                 </Link>
-                <div className="mt-2" style={{width: "130px"}}>
-                    <Link key={blog.category?.id} href={`/categories/${blog.category?.id}`} className="text-indigo-500">
-                        <span className="group bg-gray-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out transform hover:bg-indigo-100 hover:text-indigo-500 hover:scale-105">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5 text-gray-400 mr-2 group-hover:text-indigo-400"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M5 13l4 4L19 7"
-                                />
-                            </svg>
-                            {blog.category?.name}
-                        </span>
+                <div className="p-5">
+                    <Link href={`/blogs/${blog.id}`}>
+                    <h5 className="mb-2 lg:text-lg font-bold tracking-tight text-gray-900 truncate md:text-base sm:text-base">
+                        {blog.title}
+                    </h5>
                     </Link>
-                </div>
-                <div className="mt-2">
-                    <ul className="list-disc list-inside text-gray-300">
-                        {blog.tags?.map((tag) => (
-                            <span key={tag.id} className="inline-block bg-indigo-500 text-white px-2 py-1 rounded-full text-sm mr-2 mb-2">
-                                <Link href={`/tags/${tag.id}`}>
-                                    {tag.name}
-                                </Link>
+                    <div className="mt-2" style={{width: "130px"}}>
+                        <Link key={blog.category?.id} href={`/categories/${blog.category?.id}`} className="text-indigo-500">
+                            <span className="group bg-gray-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out transform hover:bg-indigo-100 hover:text-indigo-500 hover:scale-105">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5 text-gray-400 mr-2 group-hover:text-indigo-400"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M5 13l4 4L19 7"
+                                    />
+                                </svg>
+                                {blog.category?.name}
                             </span>
-                        ))}
-                    </ul>
+                        </Link>
+                    </div>
+                    <div className="mt-2">
+                        <ul className="list-disc list-inside text-gray-300">
+                            {blog.tags?.map((tag) => (
+                                <span key={tag.id} className="inline-block bg-indigo-500 text-white px-2 py-1 rounded-full text-sm mr-2 mb-2">
+                                    <Link href={`/tags/${tag.id}`}>
+                                        {tag.name}
+                                    </Link>
+                                </span>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
-              </div>
+                </div>
             </div>
-          </div>
-        );
-      })}
+            );
+        })}
+      </div>
     </>
   );
 }
