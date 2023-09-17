4c8bb1939ee0a85f51657abdfcd3492f29bf871f
j19015 <j19015@sangi.jp>
2023-09-18 04:14:20 +0900

    src/app/tags/[tagId]/page.tsx template化

--- src/app/tags/[tagId]/page.tsx
+++ src/app/tags/[tagId]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation"
 import { getList,getTagList,getTagDetail } from "../../../../libs/microcms"
 import Sidebar from "@/components/SIdebar/Sidebar"
+import Index from "@/components/Index/Index";
 import Link from "next/link";
 
 export async function generateStaticParams(){
@@ -46,26 +47,7 @@ export default async function StaticDetailPage({
           <div className="text-center mt-1 w-full col-span-2">
             <h2 className="lg:text-5xl md:text-4xl text-3xl font-extrabold text-indigo-900 mb-6 underline">{tag_show.name}</h2>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            {filteredContents.map((blog) => {
-              return (
-                <div key={blog.id} className="w-full p-2 md:w-full lg:w-full">
-                  <div className="bg-white border border-gray-200 rounded-lg shadow-md">
-                    <Link href={`/blogs/${blog.id}`}>
-                      <img className="rounded-t-lg w-full  max-w-full" src={blog.eyecatch?.url} alt="" /> {/* 画像の高さと幅を指定 */}
-                    </Link>
-                    <div className="p-5">
-                      <Link href={`/blogs/${blog.id}`}>
-                        <h5 className="mb-2 lg:text-lg font-bold tracking-tight text-gray-900 truncate md:text-base sm:text-base">
-                          {blog.title}
-                        </h5>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          <Index contents={filteredContents}/>
         </div>
         <div className="lg:col-span-1"> {/* 通常の画面サイズでは1列分のスペースを占有 */}
           <Sidebar />
