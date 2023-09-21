a92b216f1ca6738e07c2bbee2a5b03834f794f8d
Koki Takahashi <koki-takahashi@vcube.co.jp>
2023-09-21 13:07:51 +0900

    ブログ一覧　レイアウト変更

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { getList } from "../../../libs/microcms";
 import Image from 'next/image';
+import Index from "@/components/Index/Index";
 import Sidebar from "@/components/SIdebar/Sidebar"; // Sidebarのimportを修正
 
 export default async function StaticPage() {
@@ -13,36 +14,17 @@ export default async function StaticPage() {
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4"> {/* グリッドを設定 */}
-        <div className="lg:col-span-2"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 pb-8"> {/* グリッドを設定 */}
+          <div className="lg:col-span-2"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
             <div className="text-center mt-1 w-full col-span-2">
               <h2 className="lg:text-5xl md:text-4xl text-3xl font-extrabold text-indigo-900 mb-6 underline">Blog</h2>
             </div>
-            {contents.map((blog) => {
-              return (
-                <div key={blog.id} className="w-full p-2 md:w-full lg:w-full">
-                  <div className="bg-white border border-gray-200 rounded-lg shadow-md">
-                    <Link href={`/blogs/${blog.id}`}>
-                      <img className="rounded-t-lg w-full  max-w-full" src={blog.eyecatch?.url} alt="" /> {/* 画像の高さと幅を指定 */}
-                    </Link>
-                    <div className="p-5">
-                      <Link href={`/blogs/${blog.id}`}>
-                        <h5 className="mb-2 lg:text-lg font-bold tracking-tight text-gray-900 truncate md:text-base sm:text-base">
-                          {blog.title}
-                        </h5>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+            <Index contents={contents}/>
+          </div>
+          <div className="lg:col-span-1"> {/* 通常の画面サイズでは1列分のスペースを占有 */}
+            <Sidebar />
           </div>
         </div>
-        <div className="lg:col-span-1"> {/* 通常の画面サイズでは1列分のスペースを占有 */}
-          <Sidebar />
-        </div>
-      </div>
     </>
   );
 }
