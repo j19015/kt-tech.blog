3c390c34bbad76948ffd2b21e9dd1b5d3a5f3972
j19015 <j19015@sangi.jp>
2023-09-23 01:59:12 +0900

    sitemapを追加する処理を記述

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+siteUrl: process.env.SITE_URL || 'https://kt-tech.blog',
+generateRobotsTxt: true, // (optional)
+// ...other options
+}
