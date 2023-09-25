97d09db0cb189bdfee5f997f0e8cd6f1a521e1b2
Koki Takahashi <koki-takahashi@vcube.co.jp>
2023-09-25 12:54:16 +0900

    margin paddingを調整

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4"> {/* グリッドを設定 */}
         <div className="lg:col-span-2"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
-        <div className="bg-white text-black p-8 m-4 rounded-lg">
+        <div className="bg-white text-black p-8 m-1 rounded-lg">
           <div className="text-center mt-1 w-full col-span-2">
             <h2 className="lg:text-5xl md:text-4xl text-3xl font-extrabold text-indigo-900 mb-6 underline">Introduction</h2>
           </div>
