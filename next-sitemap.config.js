1971e0da44eb392766d9b49fb5ba73911998ce59
j19015 <j19015@sangi.jp>
2023-09-26 13:43:45 +0900

    [update] 記述ミス修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-siteUrl: process.env.SITE_URL || 'hhttps://www.kt-tech.blog',
+siteUrl: process.env.SITE_URL || 'https://www.kt-tech.blog',
 generateRobotsTxt: true, // (optional)
 // ...other options
 }
