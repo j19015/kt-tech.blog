67347d1379c09667d1aa9423c3b97bb3dff5ebc2
j19015 <j19015@sangi.jp>
2023-10-02 10:31:13 +0900

    [update] md padding

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -44,7 +44,7 @@ export default async function StaticDetailPage({
     <>
     <div>
       <div className="grid grid-cols-1 lg:grid-cols-3"> {/* グリッドを設定 */}
-        <div className="lg:col-span-3 lg:p-10 p-5"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
+        <div className="lg:col-span-3 lg:p-10"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
           <div>
             <div className="p-1">
               <Link href={`/blogs/${blog.id}`}>
