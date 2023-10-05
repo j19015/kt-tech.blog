8180a24a33194c67e0d12da2c9a230f8a4361945
j19015 <j19015@sangi.jp>
2023-10-06 00:37:26 +0900

    [update] font bold

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,6 +1,6 @@
 export const Title =(props:{title: string})=>{
     return (
-        <h2 className="lg:text-5xl md:text-4xl text-3xl text-indigo-500 underline mb-6" style={{ fontFamily: "'Playfair Display', serif" }}>{props.title}</h2>
+        <h2 className="lg:text-5xl md:text-4xl text-3xl text-indigo-500 mb-6 font-bold" style={{ fontFamily: "'Playfair Display', serif" }}>{props.title}</h2>
     );
 }
 
