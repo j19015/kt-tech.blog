219ae11977a9c7acc4c13c10f05746098d7c4e45
Koki Takahashi <koki-takahashi@vcube.co.jp>
2023-10-13 01:22:18 +0900

    [add] 実務経験&& h3 css

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -80,6 +80,8 @@ export default function Home() {
             サーバレスアーキテクチャ、クリーンアーキテクチャに興味があります。
           </p>
           <h2 className="text-2xl font-semibold mt-8">技術スタック</h2>
+          <h3>実務経験あり</h3>
+          <h3>実務経験なし</h3>
           <ul className="list-disc mt-2 pl-6">
             <li className='mb-3'><strong>Language</strong> : Ruby、PHP、C、C++、JavaScript、TypeScript</li>
             <li className='mb-3'><strong>FW</strong> : Express、Rails、Next.js</li>
