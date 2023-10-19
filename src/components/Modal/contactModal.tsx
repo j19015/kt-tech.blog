bfc5cb63c10132231c8d3df1e891bdcf19fc4327
j19015 <j19015@sangi.jp>
2023-10-19 21:58:02 +0900

    [update] contact layouts

--- src/components/Modal/contactModal.tsx
+++ src/components/Modal/contactModal.tsx
@@ -10,19 +10,19 @@ const ContactModal = ({ isOpen, onClose } :ContactModalProps) => {
 
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="relative w-3/4 max-w-2xl mx-auto bg-white rounded-xl shadow-lg p-6 md:p-8 mb-10">
+      <div className="relative w-3/4 max-w-2xl mx-auto bg-white rounded-xl shadow-lg p-6 md:p-8 mb-10 content" style={{ maxHeight: 'calc(100vh - 300px)', overflowY: 'auto'}}>
         <button 
             onClick={onClose} 
-            className="absolute right-6 top-6 text-2xl text-black transition duration-300"
+            className="absolute right-6 top-6 text-2xl transition duration-300"
             >
-            <span className='p-1 hover:bg-gray-300 hover:  rounded-full'>
+            <span className='p-1 hover:  rounded-full'>
                 ✖︎
             </span>
         </button>
 
-        <h2 className="text-3xl font-semibold mb-3 text-black">Contact Us</h2>
+        <h2 className="text-3xl font-semibold mb-3">Contact Us</h2>
         <p className="text-red-500">※現在作成中のため動作しません。</p>
-        <p className="text-black mb-6"><a href="https://twitter.com/tech_koki" className='underline text-black'>Twitter</a>へ連絡をお願いします。</p>
+        <p className=" mb-6"><a href="https://twitter.com/tech_koki" className='underline'>Twitter</a>へ連絡をお願いします。</p>
 
         <form action="#" method="POST" className="space-y-4">
           <div>
@@ -30,7 +30,7 @@ const ContactModal = ({ isOpen, onClose } :ContactModalProps) => {
               type="text"
               name="name"
               placeholder="Full Name"
-              className="w-full border-b-2 focus:border-indigo-500 focus:outline-none py-2 text-black"
+              className="w-full border-b-2 focus:border-indigo-500 focus:outline-none py-2 content "
               required
             />
           </div>
@@ -40,7 +40,7 @@ const ContactModal = ({ isOpen, onClose } :ContactModalProps) => {
               type="email"
               name="email"
               placeholder="Email Address"
-              className="w-full border-b-2 focus:border-indigo-500 focus:outline-none py-2 text-black"
+              className="w-full border-b-2 focus:border-indigo-500 focus:outline-none py-2 content"
               required
             />
           </div>
@@ -50,7 +50,7 @@ const ContactModal = ({ isOpen, onClose } :ContactModalProps) => {
               name="message"
               rows={4}
               placeholder="Your Message"
-              className="w-full border-2 focus:border-indigo-500 focus:outline-none p-2 rounded-md text-black"
+              className="w-full border-2 focus:border-indigo-500 focus:outline-none p-2 rounded-md content"
               required
             ></textarea>
           </div>
