d4d631334eb9505bd51b5bfa10867637a3be7e65
Koki Takahashi <koki-takahashi@vcube.co.jp>
2024-02-14 20:28:14 +0900

    fix 不要なimportを削除

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
-import { getList } from "../../../libs/microcms";
-import Image from 'next/image';
-import Index from "@/components/Index/Index";
-import Sidebar from "@/components/SIdebar/Sidebar"; // Sidebarのimportを修正
-import Title from "@/components/Title/Title";
+import { getList } from '../../../libs/microcms';
+import Index from '@/components/Index/Index';
+import Sidebar from '@/components/SIdebar/Sidebar'; // Sidebarのimportを修正
+import Title from '@/components/Title/Title';
 
 export default async function StaticPage() {
   const { contents } = await getList();
@@ -15,17 +13,23 @@ export default async function StaticPage() {
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 pb-8"> {/* グリッドを設定 */}
-          <div className="lg:col-span-2"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
-            <div className="text-center mt-1 w-full col-span-2">
-              <Title title={`Blog`} />
-            </div>
-            <Index contents={contents}/>
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 pb-8'>
+        {' '}
+        {/* グリッドを設定 */}
+        <div className='lg:col-span-2'>
+          {' '}
+          {/* 通常の画面サイズでは2列分のスペースを占有 */}
+          <div className='text-center mt-1 w-full col-span-2'>
+            <Title title={`Blog`} />
           </div>
-          <div className="lg:col-span-1"> {/* 通常の画面サイズでは1列分のスペースを占有 */}
-            <Sidebar />
-          </div>
+          <Index contents={contents} />
         </div>
+        <div className='lg:col-span-1'>
+          {' '}
+          {/* 通常の画面サイズでは1列分のスペースを占有 */}
+          <Sidebar />
+        </div>
+      </div>
     </>
   );
 }
