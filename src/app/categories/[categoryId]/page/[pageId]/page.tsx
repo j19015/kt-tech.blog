8c89e9bfae6a3cc0cd780a333d6f181f48510f9b
Koki Takahashi <koki-takahashi@vcube.co.jp>
2024-02-14 20:30:23 +0900

    fix コード整形

--- src/app/categories/[categoryId]/page/[pageId]/page.tsx
+++ src/app/categories/[categoryId]/page/[pageId]/page.tsx
@@ -1,57 +1,54 @@
-import { notFound } from "next/navigation";
-import { getList,getCategoryList,getCategoryDetail } from "../../../../../../libs/microcms";
-import Sidebar from "@/components/SIdebar/Sidebar"; // Sidebarのimportを修正
-import Paginate from "@/components/Pagination/Paginate";
-import Index from "@/components/Index/Index";
-import Title from "@/components/Title/Title";
-import Link from "next/link"
+import { notFound } from 'next/navigation';
+import { getList, getCategoryList, getCategoryDetail } from '../../../../../../libs/microcms';
+import Sidebar from '@/components/SIdebar/Sidebar'; // Sidebarのimportを修正
+import Paginate from '@/components/Pagination/Paginate';
+import Index from '@/components/Index/Index';
+import Title from '@/components/Title/Title';
 
 const ITEMS_PER_PAGE = 6; // 1ページあたりのアイテム数
 
 export async function generateStaticParams({
-    params: { categoryId },
-  }: {
-    params: { categoryId: string};
-  }) {
+  params: { categoryId },
+}: {
+  params: { categoryId: string };
+}) {
   try {
     //ブログ取得
     const { contents } = await getList();
 
     const categoryList = await getCategoryList();
-    
-    console.log(categoryList)
 
-    const paths: { categoryId: string; pageId: string; }[] = [];
+    console.log(categoryList);
+
+    const paths: { categoryId: string; pageId: string }[] = [];
 
     // 各ページのパスを生成
-    categoryList.contents.map((category)=>{
-        const filteredContents = contents.filter((blog) => blog.category?.id === category.id);
-        console.log(category.name,filteredContents.length)
-        const totalPages = Math.ceil(filteredContents.length / ITEMS_PER_PAGE);
-        for (let page = 1; page <= totalPages; page++) {
-            paths.push({
-                categoryId: category.id,
-                pageId: String(page)
-            });
-        }
-
+    categoryList.contents.map((category) => {
+      const filteredContents = contents.filter((blog) => blog.category?.id === category.id);
+      console.log(category.name, filteredContents.length);
+      const totalPages = Math.ceil(filteredContents.length / ITEMS_PER_PAGE);
+      for (let page = 1; page <= totalPages; page++) {
+        paths.push({
+          categoryId: category.id,
+          pageId: String(page),
+        });
+      }
     });
-    
-    console.log(paths)
+
+    console.log(paths);
 
     return [...paths];
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error('Error fetching data:', error);
     return [];
   }
 }
 
 export default async function StaticPaginationPage({
-  params: { categoryId ,pageId},
+  params: { categoryId, pageId },
 }: {
-  params: { categoryId: string,pageId: string };
+  params: { categoryId: string; pageId: string };
 }) {
-
   if (!pageId) {
     notFound();
   }
@@ -73,28 +70,38 @@ export default async function StaticPaginationPage({
     const contentSlice = filteredContents.slice(startIndex, endIndex);
 
     //category取得
-    const category = await getCategoryDetail(categoryId)
+    const category = await getCategoryDetail(categoryId);
 
     // コンテンツを表示するロジックをここに追加
 
     return (
       <>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 pb-8"> {/* グリッドを設定 */}
-          <div className="lg:col-span-2"> {/* 通常の画面サイズでは2列分のスペースを占有 */}
-            <div className="text-center mt-1 w-full col-span-2">
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 pb-8'>
+          {' '}
+          {/* グリッドを設定 */}
+          <div className='lg:col-span-2'>
+            {' '}
+            {/* 通常の画面サイズでは2列分のスペースを占有 */}
+            <div className='text-center mt-1 w-full col-span-2'>
               <Title title={category.name} />
             </div>
-            <Index contents={contentSlice}/>
-            <Paginate currentPage={Number(pageId)} totalPage={Math.ceil(filteredContents.length/6)} kind={`/categories/${categoryId}`}></Paginate>
+            <Index contents={contentSlice} />
+            <Paginate
+              currentPage={Number(pageId)}
+              totalPage={Math.ceil(filteredContents.length / 6)}
+              kind={`/categories/${categoryId}`}
+            ></Paginate>
           </div>
-          <div className="lg:col-span-1"> {/* 通常の画面サイズでは1列分のスペースを占有 */}
+          <div className='lg:col-span-1'>
+            {' '}
+            {/* 通常の画面サイズでは1列分のスペースを占有 */}
             <Sidebar />
           </div>
         </div>
       </>
     );
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error('Error fetching data:', error);
     return (
       <>
         <p>Error fetching data.</p>
