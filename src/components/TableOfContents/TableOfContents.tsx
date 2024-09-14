fefab35899f115be884a44a6fbd6afc5c00fd573
j19015 <j19015@sangi.jp>
2024-09-15 03:42:45 +0900

    fix: 目次機能の改善

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -1,31 +1,42 @@
 'use client';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 export const TableOfContents = ({ toc }: { toc: any }) => {
   const [isOpen, setOpen] = useState<boolean>(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      contentRef.current.style.height = isOpen ? `${contentRef.current.scrollHeight}px` : '0px';
+    }
+  }, [isOpen]);
 
   return (
     <div className='lg:hidden border-2 rounded-lg table-contents m-5 mt-10 p-1'>
-      <div className='flex justify-between px-3 py-2'>
+      <div className='flex justify-between px-3 py-2' onClick={() => setOpen(!isOpen)}>
         <h1 className='text-lg font-bold' style={{ marginTop: '0px !important' }}>
           目次
         </h1>
-        <button onClick={() => setOpen(!isOpen)} className='text-lg'>
+        <span className='text-lg'>
           <FontAwesomeIcon icon={isOpen ? faMinus : faPlus} />
-        </button>
+        </span>
       </div>
-      <div className={isOpen ? 'block' : 'hidden'}>
+      <div
+        ref={contentRef}
+        className='overflow-hidden transition-height duration-500 ease-in-out'
+        style={{ height: '0px' }}
+      >
         <ul className='pl-2'>
           {toc.map((data: any) => (
             <a key={data.id} href={`#${data.id}`}>
               <li
                 className={`${
-                  data.tag === 'h2' ? 'ml-5' : data.tag === 'h3' ? 'ml-10' : 'ml-1'
+                  data.tag === 'h2' ? 'ml-5' : data.tag === 'h3' ? 'ml-15' : 'ml-1'
                 } mb-2 hover:bg-gray-500 rounded`}
               >
-                {data.tag === 'h1' ? data.text : '-' + data.text}
+                {data.tag === 'h1' ? '・' + data.text : '-' + data.text}
               </li>
             </a>
           ))}
