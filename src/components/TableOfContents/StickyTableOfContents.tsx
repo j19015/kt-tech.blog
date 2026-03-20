c211577ca24faa907325b01f348e291f2d009627
j19015 <koki.weben.main@gmail.com>
2026-03-20 19:18:41 +0900

    perf: remove unused deps (framer-motion, react-icons, styled-components), add WebP eyecatch conversion

--- src/components/TableOfContents/StickyTableOfContents.tsx
+++ src/components/TableOfContents/StickyTableOfContents.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl } from '@fortawesome/free-solid-svg-icons';
-import { motion, AnimatePresence } from 'framer-motion';
+
 
 interface TocItem {
   id: string;
